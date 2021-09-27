ca9139b7cf20fef37091c8c6e5d97f2db6f87597
izabela-goes <62080288+izabela-goes@users.noreply.github.com>
2021-09-27 14:44:49 -0300

    ProjBoletim

---
+++ ProjMvcBoletim/Models/Disciplina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjMvcBoletim.Models
+{
+    public class Disciplina
+    {
+        public int Id { get; set; }
+        public string NomeDisciplina { get; set; }
+    }
+}
